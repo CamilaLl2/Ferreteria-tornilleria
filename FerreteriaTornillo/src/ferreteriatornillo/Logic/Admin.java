92725988ba322ac9fe0597348b64e00b9eec7f9d
John Marlon Pérez <126036039+johnMarlonxxX@users.noreply.github.com>
2023-12-04 11:12:08 -0500

    Delete FerreteriaTornillo directory

--- FerreteriaTornillo/src/ferreteriatornillo/Logic/Admin.java
+++
@@ -1,9 +0,0 @@
-
-package ferreteriatornillo.Logic;
-
-public class Admin extends Users {
-    
-    public Admin(String id, String name, String password) {
-        super(id, name, password);
-    }
-}
